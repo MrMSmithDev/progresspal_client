2cf2eac5fa447aabffbae50b61e341809ba17bdb
Michael <MichaelSmith90Dev@gmail.com>
2025-01-25 16:15:23 +0000

    Resolve issue in setting custom colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AuthProvider } from '@hooks/useAuth';
 import { ModalProvider } from '@hooks/useModal';
 import { AppProps } from 'next/app';
 
+import '@styles/globals.css';
 import '@styles/reset.css';
-import '@styles/global.css';
+import useTheme from '@hooks/useTheme';
 
 function App({ Component, pageProps }: AppProps) {
+
+    useEffect(() => {
+        document.body.classList.add('bg-background', 'dark:bg-background-dark');
+    }, [])
+
   return (
     <React.StrictMode>
       <ModalProvider>
         <AuthProvider>
-          <Component {...pageProps} />;
+          <Component
+            className="bg-background dark:bg-background-dark"
+            {...pageProps}
+          />
         </AuthProvider>
       </ModalProvider>
     </React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+console.log('Reading config');
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{css, scss}',
   ],
   theme: {
     extend: {},
   },
+  darkMode: 'class',
   plugins: [],
 };
