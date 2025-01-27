ad554821bfc36e98bed60d8d106f6b488b139cd6
Michael <MichaelSmith90Dev@gmail.com>
2025-01-27 12:09:14 +0000

    Add and export additional svg icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,9 @@
 import { default as CloseIcon } from './CloseIcon';
 import { default as DownIcon } from './DownIcon';
+import { default as DumbbellIcon } from './DumbbellIcon';
 import { default as GitHubIcon } from './GitHubIcon';
+import { default as HomeIcon } from './HomeIcon';
+import { default as SettingsIcon } from './SettingsIcon';
+import { default as WeightScaleIcon } from './WeightScaleIcon';
 
-export { CloseIcon, DownIcon, GitHubIcon };
+export { CloseIcon, DownIcon, DumbbellIcon, GitHubIcon, HomeIcon, SettingsIcon, WeightScaleIcon };
