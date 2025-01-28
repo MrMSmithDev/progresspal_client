60a90d81139861fe8f20410b73f76155253245be
Michael <MichaelSmith90Dev@gmail.com>
2025-01-28 13:38:51 +0000

    Restructure ThemeToggle button

--- src/components/buttons/ThemeToggle.tsx
+++ src/components/buttons/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 import useTheme from '@hooks/useTheme';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import style from '@styles/themeToggle.module.scss';
 import { DarkModeIcon, LightModeIcon } from '@components/icons';
@@ -8,9 +8,17 @@ const ThemeToggle: React.FC = () => {
   const [theme, toggleTheme] = useTheme();
 
   return (
-    <button className="h-[50px] w-[50px] p-0 rounded-lg flex justify-center items-center">
-      <LightModeIcon />
-      <DarkModeIcon />
+    <button
+      className="h-[34px] w-[34px] p-0 rounded-lg flex justify-center items-center cursor-pointer"
+      onClick={toggleTheme}
+    >
+      <div className={theme === 'light' ? 'hidden' : ''}>
+        <LightModeIcon />
+      </div>
+
+      <div className={theme === 'light' ? '' : 'hidden'}>
+        <DarkModeIcon />
+      </div>
     </button>
   );
 };
