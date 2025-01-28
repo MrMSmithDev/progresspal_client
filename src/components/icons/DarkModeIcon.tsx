d062806d5a0041a2fd90ebcf3efaf13ec34a6900
Michael <MichaelSmith90Dev@gmail.com>
2025-01-28 11:59:40 +0000

    Add IconProps for dynamic classing

--- src/components/icons/DarkModeIcon.tsx
+++ src/components/icons/DarkModeIcon.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { IconProps } from './types';
 
-const DarkModeIcon: React.FC = () => {
+const DarkModeIcon: React.FC<IconProps> = ({ className }) => {
   return (
     <svg
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       height="24px"
       viewBox="0 -960 960 960"
--- src/components/icons/LightModeIcon.tsx
+++ src/components/icons/LightModeIcon.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { IconProps } from './types';
 
-const LightModeIcon: React.FC = () => {
+const LightModeIcon: React.FC<IconProps> = ({ className }) => {
   return (
     <svg
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       height="24px"
       viewBox="0 -960 960 960"
