b1a946d82c7d623d54dbde260d44ab3fabb76dee
Michael <MichaelSmith90Dev@gmail.com>
2025-02-12 11:57:41 +0000

    Implement 404 not Found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Header } from '@components/index';
+import { useAuth } from '@hooks/useAuth';
+import Link from 'next/link';
+
+const NotFound: React.FC = () => {
+  const { token } = useAuth();
+
+  return (
+    <>
+      {token ? <Header /> : null}
+      <main className="flex justify-center items-center w-full h-[100lvh]">
+        <div className="relative flex flex-col shadow-md rounded items-center w-100 m-4 p-4 bg-secondary-background dark:bg-secondary-background-dark">
+          <h1 className="text-4xl tracking-wider">404</h1>
+          <p className="text-xl">Page not found</p>
+          {token ? (
+            <Link
+              href="/dashboard"
+              className="text-sm mb-4 dark:text-gray-100 inline-block rounded border-solid border-2 border-primary py-1 px-3 tracking-wider"
+            >
+              Go home
+            </Link>
+          ) : (
+            <div className="flex gap-3 mt-3">
+              <Link
+                href="/"
+                className="text-sm mb-4 dark:text-gray-100 inline-block rounded border-solid border-2 border-primary py-1 px-3 tracking-wider"
+              >
+                Sign up
+              </Link>
+              <span>or</span>
+              <Link
+                href="/signin"
+                className="text-sm mb-4 dark:text-gray-100 inline-block rounded border-solid border-2 border-primary py-1 px-3 tracking-wider"
+              >
+                Sign in
+              </Link>
+            </div>
+          )}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default NotFound;
