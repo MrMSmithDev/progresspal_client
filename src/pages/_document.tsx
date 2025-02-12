93168c7c23c58b0c1bfb6823b484ecc84a1929b9
Michael <MichaelSmith90Dev@gmail.com>
2025-02-12 17:39:05 +0000

    Add _document.tsx for theme setting and meta

---
+++ src/pages/_document.tsx
@@ -0,0 +1,39 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    console.log("Using document");
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  const storedTheme = localStorage.getItem('theme');
+                  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+                  const initialTheme = storedTheme || systemTheme;
+                  document.documentElement.classList.add(initialTheme);
+                })();
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
