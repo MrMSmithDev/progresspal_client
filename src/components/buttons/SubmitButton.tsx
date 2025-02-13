1c7e221e70cfc86eb6eb4ce9565dec11ee6463bb
Michael <MichaelSmith90Dev@gmail.com>
2025-02-13 16:16:11 +0000

    Add className props to submitButton and additional exports to index

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 interface SubmitButtonInterface {
   func: (args: any) => any;
   text: string;
+  className?: string
 }
 
-const SubmitButton: React.FC<SubmitButtonInterface> = ({ func, text }) => {
-  return <button onClick={func} className="rounded bg-button dark:bg-button-dark text-white p-1 w-30 cursor-pointer tracking-wider">{text}</button>;
+const SubmitButton: React.FC<SubmitButtonInterface> = ({ func, text, className }) => {
+  return <button onClick={func} className={`${className} rounded bg-button dark:bg-button-dark text-white p-1 w-30 cursor-pointer tracking-wider`}>{text}</button>;
 };
 
 export default SubmitButton;
--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,9 +1,21 @@
 import { default as CancelButton } from './CancelButton';
 import { default as DemoButton } from './DemoButton';
 import { default as GenericButton } from './GenericButton';
+import { default as NewWeightButton } from './NewWeightButton';
+import { default as NewWorkoutButton } from './NewWorkoutButton';
 import { default as SignOutButton } from './SignOutButton';
 import { default as SubmitButton } from './SubmitButton';
 import { default as ThemeToggle } from './ThemeToggle';
-import { default as UnitSelectorButtons } from './UnitSelectorButtons'
+import { default as UnitSelectorButtons } from './UnitSelectorButtons';
 
-export { CancelButton, DemoButton, GenericButton, SignOutButton, SubmitButton, ThemeToggle, UnitSelectorButtons };
+export {
+  CancelButton,
+  DemoButton,
+  GenericButton,
+  NewWeightButton,
+  NewWorkoutButton,
+  SignOutButton,
+  SubmitButton,
+  ThemeToggle,
+  UnitSelectorButtons,
+};
