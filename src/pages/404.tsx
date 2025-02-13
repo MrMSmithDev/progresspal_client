e366bacde2fe3a40db053796439ff565677b4ecd
Michael <MichaelSmith90Dev@gmail.com>
2025-02-13 12:46:24 +0000

    Adjust spacing

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -11,8 +11,8 @@ const NotFound: React.FC = () => {
       {token ? <Header /> : null}
       <main className="flex justify-center items-center w-full h-[100lvh]">
         <div className="relative flex flex-col shadow-md rounded items-center w-100 m-4 p-4 bg-secondary-background dark:bg-secondary-background-dark">
-          <h1 className="text-4xl tracking-wider">404</h1>
-          <p className="text-xl">Page not found</p>
+          <h1 className="text-4xl tracking-wider dark:text-gray-100">404</h1>
+          <p className="text-xl dark:text-gray-100 mb-3">Page not found</p>
           {token ? (
             <Link
               href="/dashboard"
