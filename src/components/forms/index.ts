55fe26c684950da01a738e224cc405aacb4e030b
Michael <MichaelSmith90Dev@gmail.com>
2025-02-14 13:10:22 +0000

    Export workout form fields

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,8 +1,19 @@
+import { default as CardioFields } from './CardioFields';
 import { default as ModalFormBg } from './ModalFormBg';
 import { default as NewWorkoutForm } from './NewWorkoutForm';
 import { default as SignInForm } from './SignInForm';
 import { default as SignupForm } from './SignupForm';
+import { default as StrengthFields } from './StrengthFields';
 import { default as UpdateEmailForm } from './UpdateEmailForm';
 import { default as UpdatePasswordForm } from './UpdatePasswordForm';
 
-export { ModalFormBg, NewWorkoutForm, SignInForm, SignupForm, UpdateEmailForm, UpdatePasswordForm };
+export {
+  CardioFields,
+  ModalFormBg,
+  NewWorkoutForm,
+  SignInForm,
+  SignupForm,
+  StrengthFields,
+  UpdateEmailForm,
+  UpdatePasswordForm,
+};
