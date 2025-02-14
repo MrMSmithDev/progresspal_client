9cf2c6a53140828208634511e89bdedf6e0f8828
Michael <MichaelSmith90Dev@gmail.com>
2025-02-14 13:07:19 +0000

    Resolve closeModal click when off target

--- src/components/forms/ModalFormBg.tsx
+++ src/components/forms/ModalFormBg.tsx
@@ -1,4 +1,3 @@
-import { CLIENT_STATIC_FILES_RUNTIME_POLYFILLS_SYMBOL } from 'next/dist/shared/lib/constants';
 import React, { useEffect, useRef } from 'react';
 
 interface ModalFormBgProps {
@@ -6,19 +5,38 @@ interface ModalFormBgProps {
   closeModal: () => void;
 }
 
-const ModalFormBg: React.FC<ModalFormBgProps> = ({ children }) => {
+const ModalFormBg: React.FC<ModalFormBgProps> = ({ children, closeModal }) => {
   const ref = useRef(null);
 
   useEffect(() => {
-    // close on click outside
-  }, [ref]);
+    setTimeout(() => {
+      if (ref.current) {
+        function closeModalCallback(event: MouseEvent) {
+          const modal = ref.current;
+          const workoutButton = document.querySelector('.workout-button');
+
+          if (
+            modal &&
+            !modal.contains(event.target as Node) &&
+            workoutButton &&
+            !workoutButton.contains(event.target as Node)
+          ) {
+            closeModal();
+            window.removeEventListener('click', closeModalCallback);
+          }
+        }
+        window.addEventListener('click', closeModalCallback);
+
+        return () => window.removeEventListener('click', closeModalCallback);
+      }
+    }, 0);
+  }, []);
 
   return (
     <div
-      ref={ref}
       className="flex justify-center items-center p-5 min-h-[100lvh] w-[100vw] modal-form-bg absolute top-0 left-0"
     >
-      <div className="flex-grow m-5 w-full min-h-200 max-w-200 bg-background dark:bg-background-dark rounded p-3">
+      <div ref={ref} className="flex-grow m-5 w-full min-h-200 max-w-200 bg-background dark:bg-background-dark rounded p-3">
         {children}
       </div>
     </div>
