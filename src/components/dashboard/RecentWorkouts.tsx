1c2e50720166c8b4f0aaf027e631bc4df00afb5a
Michael <MichaelSmith90Dev@gmail.com>
2025-02-19 15:10:59 +0000

    Add key prop for lastSevenWorkouts

--- src/components/dashboard/RecentWorkouts.tsx
+++ src/components/dashboard/RecentWorkouts.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { Workout } from 'src/customTypes';
 import formatDate from '@utils/formatDate';
 import ExerciseTable from '@components/utils/ExerciseTable';
+import useUnits from '@hooks/useUnits';
+import { convertDistanceUnits, convertWeightUnits } from '@utils/convertUnits';
 
 type weeksWorkout = { day: number; workoutOnDay: boolean };
 
@@ -12,17 +14,7 @@ interface RecentWorkoutsProps {
 
 const RecentWorkouts: React.FC<RecentWorkoutsProps> = ({ workouts }) => {
   const [loading, setLoading] = useState<boolean>(true);
-
-  const fiveMostRecent: Workout[] = useMemo(() => {
-    if (workouts.length <= 0) return [];
-
-    return [...workouts]
-      .sort(
-        (a: Workout, b: Workout) =>
-          new Date(b.date).getTime() - new Date(a.date).getTime()
-      )
-      .slice(0, 5);
-  }, [workouts]);
+  const [units] = useUnits();
 
   const workoutsThisWeek: weeksWorkout[] = useMemo(() => {
     const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
@@ -50,9 +42,47 @@ const RecentWorkouts: React.FC<RecentWorkoutsProps> = ({ workouts }) => {
     return streakArr.reverse();
   }, [workouts]);
 
+  const fiveMostRecent: Workout[] = useMemo(() => {
+    if (workouts.length <= 0) return [];
+
+    const sortedArr = [...workouts]
+      .sort(
+        (a: Workout, b: Workout) =>
+          new Date(b.date).getTime() - new Date(a.date).getTime()
+      )
+      .slice(0, 5);
+
+    const convertedArr = sortedArr.map((workout) => ({
+      ...workout,
+      exercises: workout.exercises.map((exercise) => {
+        return exercise.type === 'cardio'
+          ? {
+              name: exercise.name,
+              time: exercise.time,
+              distance:
+                workout.unit === units
+                  ? exercise.distance
+                  : convertDistanceUnits(exercise.distance, workout.unit),
+            }
+          : {
+              name: exercise.name,
+              sets: exercise.sets.map((set) => ({
+                reps: set.reps,
+                weight:
+                  workout.unit === units
+                    ? set.weight
+                    : convertWeightUnits(set.weight, workout.unit),
+              })),
+            };
+      }),
+    }));
+
+    return sortedArr;
+  }, [workouts, units]);
+
   useEffect(() => {
-    if (workoutsThisWeek) setLoading(false);
-  }, [workoutsThisWeek]);
+    if (workoutsThisWeek && fiveMostRecent) setLoading(false);
+  }, [workoutsThisWeek, fiveMostRecent]);
 
   if (loading)
     return (
@@ -66,9 +96,10 @@ const RecentWorkouts: React.FC<RecentWorkoutsProps> = ({ workouts }) => {
       <div className="flex flex-col items-center w-full">
         <h2 className="font-bold mb-2">LAST SEVEN DAYS</h2>
         <div className="mb-3 flex w-full">
-          {workoutsThisWeek.map((workout) => (
+          {workoutsThisWeek.map((workout, i) => (
             <div
-              className={`${workout.workoutOnDay ? 'bg-primary' : 'bg-gray-500'} h-5 flex-grow first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg`}
+              key={i}
+              className={`${workout.workoutOnDay ? 'bg-primary' : 'bg-gray-500'} h-5 flex-grow first:rounded-tl-lg border-x-2 border-x-solid border-x-gray-900 first:border-l-0 first:rounded-bl-lg last:border-r-0 last:rounded-tr-lg last:rounded-br-lg`}
             ></div>
           ))}
         </div>
@@ -78,14 +109,21 @@ const RecentWorkouts: React.FC<RecentWorkoutsProps> = ({ workouts }) => {
         <h2 className="font-bold mb-2">RECENT WORKOUTS</h2>
         {fiveMostRecent.length ? (
           fiveMostRecent.map((workout, i) => (
-            <div className="flex flex-col items-center border-b-primary border-b-2 border-b-solid mb-3 rounded max-h-[150px] overflow-hidden">
+            <div
+              key={`${workout.date}-${i}`}
+              className="relative flex flex-col items-center border-b-primary border-b-2 border-b-solid mb-3 rounded max-h-[150px] overflow-hidden after:content-[''] after:absolute after:bottom-0 after:left-0 after:right-0 after:h-1/4 after:bg-gradient-to-t after:from-background dark:after:from-background-dark after:to-transparent"
+            >
               <h3 className="italic mb-3 tracking-wide">
                 Workout {formatDate(workout.date)} ({workout.exercises.length}{' '}
                 exercise{workout.exercises.length > 1 ? 's' : ''})
               </h3>
-              {workout.exercises.map((exercise) => (
-                <ExerciseTable exercise={exercise} />
+              {workout.exercises.map((exercise, i) => (
+                <ExerciseTable
+                  key={`${workout.date}-${exercise.name}-${i}`}
+                  exercise={exercise}
+                />
               ))}
+              <span>See all workouts</span>
             </div>
           ))
         ) : (
